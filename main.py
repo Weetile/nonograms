4d49d7a932086064e19a8956d05656fc5cec389e
Weetile <weetile@hotmail.com>
2022-04-30 22:54:25 +0100

    Added debugging tools

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ print("=================================================================")
 print("Nonograms by Candidate 0367 for OCR A Level Computer Science 2022")
 print("=================================================================")
 filename = "heart.txt"
-
+debug = True
 # DATA STRUCTURE SECTION 1:
 # Basic input of the nonogram text file
 with open(filename) as file_in:
     nonogram = []
     for line in file_in:
         nonogram.append(line.rstrip('\n'))
+
+if debug == True:
+    print("Nonogram data structure = " + str(nonogram))
+
 # for row in nonogram:
 #     print(row) # For debugging/output purposes
 
@@ -28,8 +32,10 @@ for row in nonogram: # Iterate through each row
             consecutiveCount = 0 # ...and then reset.
     if consecutiveCount > 0: # JUST IN CASE the last value of a row is a 1
         rowHints.append(consecutiveCount) # Gives it a chance to be appended
-    hintsHorizontal.append(rowHints)        
-#print(hintsHorizontal) # For debugging/output purposes
+    hintsHorizontal.append(rowHints)
+
+if debug == True:
+    print("hintsHorizontal = " + str(hintsHorizontal)) # For debugging/output purposes
 
 # Code to generate the minimum number of squares to represent the row hints
 # This will be very important later for the GUI grid size
@@ -40,12 +46,17 @@ for row in hintsHorizontal:
         count += 1
     if count > hintsHorizontalMinimum:
         hintsHorizontalMinimum = count
-# print(hintsHorizontalMinimum) # For debugging/output purposes
+
+if debug == True:
+    print("hintsHorizontalMinimum = " + str(hintsHorizontalMinimum)) # For debugging/output purposes
 
 # STRUCTURE SECTION 2:
 # Algorithm below is to generate the vertical key/hints for the current puzzle
 
 hintsVertical  = []
+
+
+    
 
 # STRUCTURE SECTION 3:
 # Graphical user interface in Tkinter
